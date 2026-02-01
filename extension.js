1c039438d94bc8d721062557c410ee99f0c9aaac
nabahasyed <nabahasyed05@gmail.com>
2026-01-31 21:53:11 -0500

    working colour

--- extension.js
+++ extension.js
@@ -1,98 +1,96 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const { spawn } = require('child_process');
 
+// ANSI color codes
 const RED = '\x1b[31m';
 const BLUE = '\x1b[34m';
 const RESET = '\x1b[0m';
 const BOLD = '\x1b[1m';
 
+/**
+ * Creates a VS Code pseudoterminal that can render ANSI colors
+ */
+function createCompilerTerminal(label) {
+	let writeEmitter = new vscode.EventEmitter();
+
+	const pty = {
+		onDidWrite: writeEmitter.event,
+		open: () => {},
+		close: () => {}
+	};
+
+	const terminal = vscode.window.createTerminal({ name: `Compile: ${label}`, pty });
+	return { terminal, write: (data) => writeEmitter.fire(data) };
+}
+
+/**
+ * Runs a compiler and writes colored output to a pseudoterminal
+ */
 function runCompiler(command, args, label) {
-	const terminal = vscode.windowcreateTerminal(`Compile: ${label}`);
+	const { terminal, write } = createCompilerTerminal(label);
 	terminal.show();
 
-	const proc = spawn(command, args, { shell: true });
+	const proc = spawn(command, args, { shell: false });
 
-	performance.setResourceTimingBufferSize.on('data', data => {
-		terminal.sendText(data.toString(), false);
-	});
+	proc.stdout.on('data', data => write(data.toString()));
 
 	proc.stderr.on('data', data => {
 		const lines = data.toString().split('\n');
 
 		lines.forEach(line => {
-			if(!line.trim()) return;
+			if (!line.trim()) return;
+
+			let colored = line + '\r\n';
 
 			if (/error/i.test(line)) {
-				terminal.sendText(`${BOLD}${RED}${line}${RESET}`, false);
+				colored = `${BOLD}${RED}${line}${RESET}\r\n`;
+			} else if (/warning/i.test(line)) {
+				colored = `${BLUE}${line}${RESET}\r\n`;
 			}
-			else if (/warning/i.text(line)){
-				terminal.sendText(`${YELLOW}${line}${RESET}`, false);
-			}
-			else {
-				terminal.sendText(line, false);
-			}
+
+			write(colored);
 		});
 	});
 
 	proc.on('close', code => {
-		if (code === 0) {
-			terminal.sendText(`${BOLD}✔ Compilation succeeded${RESET}`, false);
-
-		}
-		else {
-			terminal.sendText(`${BOLD}${RED}✖ Compilation failed (exit code ${code})${RESET}`, false);
-		}
+		const msg =
+			code === 0
+				? `${BOLD}${BLUE}✔ Compilation succeeded${RESET}\r\n`
+				: `${BOLD}${RED}✖ Compilation failed (exit code ${code})${RESET}\r\n`;
+		write(msg);
 	});
-
 }
-
-
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	// Register GCC command
+	context.subscriptions.push(
+		vscode.commands.registerCommand('compilesave.gcc', () => {
+			const file = vscode.window.activeTextEditor?.document.fileName;
+			if (!file) return;
+			runCompiler('gcc', [file], 'GCC');
+		})
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('compilesave.compileSave', function () {
-		// The code you place here will be executed every time your command is executed
-		context.subscriptions.push(
-        	vscode.commands.registerCommand('compilesave.gcc', () => {
-				const file = vscode.window.activeTextEditor?.document.fileName;
-				if (!file) return;
+	// Register JavaC command
+	context.subscriptions.push(
+		vscode.commands.registerCommand('compilesave.javac', () => {
+			const file = vscode.window.activeTextEditor?.document.fileName;
+			if (!file) return;
+			runCompiler('javac', [file], 'JavaC');
+		})
+	);
 
-				runCompiler('gcc', [file], 'GCC');
-			}),
-
-			vscode.commands.registerCommand('compilesave.javac', () => {
-				const file = vscode.window.activeTextEditor?.document.fileName;
-				if (!file) return;
-
-				runCompiler('javac', [file], 'JavaC');
-			}),
-
-			vscode.commands.registerCommand('compilesave.tsc', () => {
-				runCompiler('tsc', [], 'TypeScript');
-			})
-		);
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CompileSave!');
-	});
-
-	context.subscriptions.push(disposable);
+	// Register TypeScript command
+	context.subscriptions.push(
+		vscode.commands.registerCommand('compilesave.tsc', () => {
+			runCompiler('tsc', [], 'TypeScript');
+		})
+	);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
-module.exports = {
-	activate,
-	deactivate
-}
+module.exports = { activate, deactivate };
